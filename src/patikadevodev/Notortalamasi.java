a4ecfa1ce08974f869bfaaff34bb1bfdaaa80898
Nedim Ertit <69191550+ertitN@users.noreply.github.com>
2022-09-11 17:52:14 +0300

    Update Notortalamasi.java
    0 ile 100 arasında olan dersler ortalamaya dahil edilmedi.

--- src/patikadevodev/Notortalamasi.java
+++ src/patikadevodev/Notortalamasi.java
@@ -3,32 +3,65 @@ import java.util.Scanner;
 
 public class Notortalamasi {
     public static void main(String[] args) {
-        int mat,fizik,kimya,turkce,tarih,muzik;
-        double notortalamasi;
-        String sonuc;
+
+        /* ÖDEV
+        Sınıf geçme durumunu hesaplayınız. Geçme Notu: 55
+        Eğer girilen ders notları 0 veya 100 arasında değil ise ortalamaya katılmasın. */
+
+
+        int mat,turkce,kimya,muzik,fizik;
+        double notortalamasi = 0;
+        int counter = 0; // Sadece 0 ile 100 arasında olan notları ortalamaya dahil etmek için oluşturduğum değişken.
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Matematik notunuzu giriniz: ");
         mat = input.nextInt();
-
-        System.out.print("Fizik notunuzu giriniz: ");
-        fizik = input.nextInt();
-
-        System.out.print("Kimya notunuzu giriniz: ");
-        kimya = input.nextInt();
+        if(mat>=0 && mat<=100){
+            notortalamasi += mat;
+            counter++;
+        }
 
         System.out.print("Türkçe notunuzu giriniz: ");
         turkce = input.nextInt();
 
-        System.out.print("Tarih notunuzu giriniz: ");
-        tarih = input.nextInt();
+        if(turkce >= 0 && turkce <= 100){
+            notortalamasi += turkce;
+            counter++;
+        }
+
+        System.out.print("Kimya notunuzu giriniz: ");
+        kimya = input.nextInt();
+
+        if(kimya>=0 && kimya<=100){
+            notortalamasi += kimya;
+            counter++;
+        }
+
+        System.out.print("Fizik notunuzu giriniz: ");
+        fizik = input.nextInt();
+        
+        if(fizik>=0 && fizik<=100){
+            notortalamasi += fizik;
+            counter++;
+        }
+
 
         System.out.print("Müzik notunuzu giriniz: ");
         muzik = input.nextInt();
 
-        notortalamasi = (mat + fizik + kimya + turkce + tarih + muzik)/6;
-        sonuc = (notortalamasi > 60) ? "Sınıfı geçti" : "Sınıfta Kaldı";
-        System.out.println("Ortalamanız: "+notortalamasi+"\nGeçme Durumu: "+sonuc);
+        if(muzik>=0 && muzik<=100){
+            notortalamasi += muzik;
+            counter++;
+        }
+
+        notortalamasi /= counter;
+
+        if(notortalamasi >= 55){
+            System.out.println("Ortalamanız: "+notortalamasi+"\nTebrikler. Sınıfı geçtiniz");
+        } else{
+            System.out.println("Ortalamanız: "+notortalamasi+"\nSınıfta kaldınız.");
+        }
+
     }
 }
