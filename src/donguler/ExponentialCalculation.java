75fd26d2c4498aab4853ff9184001452a6bb21d5
Nedim Ertit <69191550+ertitN@users.noreply.github.com>
2022-09-18 01:02:30 +0300

    Add files via upload

---
+++ src/donguler/ExponentialCalculation.java
@@ -0,0 +1,23 @@
+package donguler;
+import java.util.Scanner;
+
+public class ExponentialCalculation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num,power,result = 1;
+
+        System.out.print("Üssünü almak istediğiniz sayıyı giriniz: ");
+        num = scan.nextInt();
+
+        System.out.print("Sayının kuvvetini giriniz: ");
+        power = scan.nextInt();
+
+        for(int i = 1;i<=power;i++){
+
+                result = result * num;
+        }
+
+        System.out.println(num+"^"+power+"= "+result);
+    }
+}
