03ebf75a03c9a9342b61e1365320dea2d0402643
Nedim Ertit <69191550+ertitN@users.noreply.github.com>
2022-09-22 17:17:10 +0300

    Add files via upload

---
+++ src/donguler/DiamondWithStars.java
@@ -0,0 +1,40 @@
+package donguler;
+
+public class DiamondWithStars {
+    public static void main(String[] args) {
+
+        int n = 7;
+        int sum = -1;
+
+        for(int i =1;i<=n;i++){
+           sum +=2 ;
+
+            for(int k =1;k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=sum;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+
+            }
+        for(int i=1;i<=n;i++){
+            sum -= 2;
+
+
+            for(int k=1;k<=i;k++){
+                System.out.print(" ");
+
+            }
+
+            for(int j= sum;j>=1;j--){
+
+                System.out.print("*");
+            }
+
+
+            System.out.println("");
+        }
+        }
+    }
+
